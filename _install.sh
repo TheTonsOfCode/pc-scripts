108b875e73c6cfc6ea6766804b9f6e8ebe995c80
TheTonsOfCode <pawe9145@gmail.com>
2025-04-18 11:54:41 +0200

    clean-js

--- _install.sh
+++ _install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 #brew install jq
-chmod +x ./ipack.sh
-sudo cp ./ipack.sh /usr/local/bin/ipack
+chmod +x ./ipack
+sudo cp ./ipack /usr/local/bin/ipack
+chmod +x ./clean-js
+sudo cp ./clean-js /usr/local/bin/clean-js
