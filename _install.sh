4be95ccc71ec8ed5e70a197d8974cade003c1e86
TheTonsOfCode <pawe9145@gmail.com>
2025-04-18 13:58:48 +0200

    rename to localpack

--- _install.sh
+++ _install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 #brew install jq
-chmod +x ./ipack
-sudo cp ./ipack /usr/local/bin/ipack
+
+chmod +x ./localpack
+sudo cp ./localpack /usr/local/bin/localpack
 chmod +x ./clean-js
 sudo cp ./clean-js /usr/local/bin/clean-js
